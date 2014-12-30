531776e1400bf5c3d04faba0f8ce31ddab99cfb1
yulijia <yulj2010@gmail.com>
2014-12-30 20:20:41 +0800

    block spam bot referral traffic

---
+++ _includes/ban.js
@@ -0,0 +1,8 @@
+<script type='text/javascript'>
+var blocklink = ['http://darodar.com','http://buttons-for-website.com','http://ilovevitaly.co','http://semalt.com/'];
+for (var b = blocklink.length; b--;) {
+  if (document.referrer.match(blocklink[b]))
+    window.location = "http://www.google.com";
+}
+</script>
+
