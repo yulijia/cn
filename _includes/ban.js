e4a4feba2952a81aa8167fea2519bd9a0847180a
yulijia <yulj2010@gmail.com>
2015-01-01 12:57:07 +0800

    update block domain list

--- _includes/ban.js
+++ _includes/ban.js
@@ -1,5 +1,5 @@
 <script type='text/javascript'>
-var blocklink = ['http://darodar.com','http://buttons-for-website.com','http://ilovevitaly.co','http://semalt.com/'];
+var blocklink = ['http://darodar.com','http://buttons-for-website.com','http://ilovevitaly.co','http://semalt.com','http://priceg.com'];
 for (var b = blocklink.length; b--;) {
   if (document.referrer.match(blocklink[b]))
     window.location = "http://www.google.com";
